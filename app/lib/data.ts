8a4ac12d324abee685913e4e5a941b52deeb742b
Jason Rathgeber <jrathgeber@yahoo.com>
2024-06-06 14:23:35 -0400

    my changes 2

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,5 @@
 import { sql } from '@vercel/postgres';
+import { unstable_noStore as noStore } from 'next/cache';
 import {
   CustomerField,
   CustomersTableType,
@@ -14,18 +15,23 @@ export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
+  noStore();
+
   try {
-    // Artificially delay a response for demo purposes.
+
+    // We artificially delay a response for demo purposes.
     // Don't do this in production :)
+    console.log('Fetching revenue data...');
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+ 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
+ 
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
+
+
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
